f019346158e3d41291ffa2c3d6cc260356d1d681
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2020-02-13 09:24:29 +0800

    Add checker for page not found

--- src/sign/sign.js
+++ src/sign/sign.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { NotFound } from '../notfound/notfound.js';
 
 class Sign extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      statusList: ['signup', 'signin'],
+    }
+  }
+
   render() {
+    const {action} = this.props.match.params;
+    const status = this.state.statusList.indexOf(action);
     return (
-      <h1>sign</h1>
+      status !== -1 ?
+        <h1>sign</h1>
+        :
+        <Route
+          component={NotFound}
+          status={404}
+        />
     );
   }
 }
