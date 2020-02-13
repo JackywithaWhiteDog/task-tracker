189db369eef3c781478ba620cb85929b478c5dc9
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2020-02-13 14:13:33 +0800

    Solve indentation

--- src/sign/sign.js
+++ src/sign/sign.js
@@ -14,19 +14,19 @@ class Sign extends React.Component {
     const status = this.state.statusList.indexOf(action);
     return (
       status !== -1 ?
-        <main>
-          {
-            status === 0 ?
-            <Link to="/sign/signin">登入</Link>
-            :
-            <Link to="/sign/signup">註冊</Link>  
-          }
-        </main>
-        :
-        <Route
-          component={NotFound}
-          status={404}
-        />
+      <main>
+        {
+          status === 0 ?
+          <Link to="/sign/signin">登入</Link>
+          :
+          <Link to="/sign/signup">註冊</Link>  
+        }
+      </main>
+      :
+      <Route
+        component={NotFound}
+        status={404}
+      />
     );
   }
 }
