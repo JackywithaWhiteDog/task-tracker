801f7bc0874a1537f03e834818ccf4f1d0e823e2
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2020-02-13 14:39:26 +0800

    Initialize firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Switch, Route } from 'react-router-dom'
+import firebase from 'firebase/app';
+import 'firebase/analytics'
+
 import { Welcome } from './welcome/welcome.js';
 import { Sign } from './sign/sign.js';
 import { App } from './app/app.js';
@@ -40,6 +43,19 @@ class Main extends React.Component {
 }
 
 // ==========================================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWFPMQ9wjnMSpQ-lzAtAvKKoDqmmUkm_c",
+  authDomain: "task-tracker-279c8.firebaseapp.com",
+  databaseURL: "https://task-tracker-279c8.firebaseio.com",
+  projectId: "task-tracker-279c8",
+  storageBucket: "task-tracker-279c8.appspot.com",
+  messagingSenderId: "93406496415",
+  appId: "1:93406496415:web:fbb760852eea1a728bc4aa",
+  measurementId: "G-4D12W0DEJC"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <Main />,
