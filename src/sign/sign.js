c2300d0b504d39e3c80b7a1aa67404c79c3ec560
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2020-02-15 17:28:30 +0800

    change Sign to functional component

--- src/sign/sign.js
+++ src/sign/sign.js
@@ -7,39 +7,32 @@ import { insureFirebase } from '../base.js';
 
 insureFirebase();
 
-class Sign extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      uiConfig: {
-        signInSuccessUrl: '/#/app/',
-        signInOptions: [
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-        ],
-        tosUrl: '/',
-        privacyPolicyUrl: '/',
-        signInFlow: 'popup',
-        callbacks: {
-          'signInSuccessWithAuthResult': (authResult, redirectUrl) => {
-            alert('Success');
-            return true;
-          },
-        }
-      }
+function Sign(props) {
+  const uiConfig = {
+    signInSuccessUrl: '/#/app/',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    tosUrl: '/',
+    privacyPolicyUrl: '/',
+    signInFlow: 'popup',
+    callbacks: {
+      'signInSuccessWithAuthResult': (authResult, redirectUrl) => {
+        alert('Success');
+        return true;
+      },
     }
-  }
+  };
 
-  render() {
-    return (
-      <main>
-        <StyledFirebaseAuth
-          uiConfig = {this.state.uiConfig}
-          firebaseAuth={firebase.auth()}
-        />
-      </main>
-    );
-  }
+  return (
+    <main>
+      <StyledFirebaseAuth
+        uiConfig = {uiConfig}
+        firebaseAuth={firebase.auth()}
+      />
+    </main>
+  );
 }
 
 export {Sign}
