63da6c80d0d849ebbf4464d64d51ffd8663da78b
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2020-02-20 23:21:00 +0800

    Set user default status when signin/signup

--- src/sign/sign.js
+++ src/sign/sign.js
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from 'firebase/app'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 import { insureFirebase } from '../base.js';
 
@@ -20,6 +21,19 @@ const Sign = () => {
     callbacks: {
       'signInSuccessWithAuthResult': (authResult, redirectUrl) => {
         console.log('signin');
+        const uid = authResult.user.uid;
+        const ref = firebase.firestore().collection('users').doc(uid);
+        console.log(ref);
+        ref.get().then(docSnapshot => {
+          if (!docSnapshot.exists) {
+            let d = new Date();
+            d.setHours(0); d.setMinutes(0); d.setSeconds(0); d.setMilliseconds(0);
+            ref.set({
+              time: d.getTime(),
+              action: null,
+            },{merge: true});
+          }
+        });
         return true;
       },
     }
